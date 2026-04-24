5a5dc20544da64a790f056ddc559326bd0f57c0c
pcdajr <pcdajr@gmail.com>
2026-04-24 02:54:44 -0300

    correção de nome

--- cypress/e2e/01_cenario_login/ct04.cy.js
+++ cypress/e2e/01_cenario_login/ct04.cy.js
@@ -4,7 +4,7 @@ describe('Página de login Saucedemo', () => {
     cy.visit('https://www.saucedemo.com/'); 
   });
 
-  it('Campos vazios - não deve entrar no sistema e informar o sdf', () => { 
+  it('Campos vazios - não deve entrar no sistema e aparecer balão de dicas', () => { 
     cy.get('[data-test="login-button"]').click() 
     cy.contains('[data-test="error"]',"Informe login e senha")  // capturando o elemento, verificando se 
                                                                 // tem a mensagem e se vísivel.
