f5681985af285f6f712ad1383eb49140c3f123b5
pcdajr <pcdajr@gmail.com>
2026-04-27 17:19:06 -0300

    criando caso teste 07 - tentando como implementar a asserção dinâmica de clicks na lista de produtos.

--- cypress/e2e/carrinho/ct07_add_cart.cy.js
+++ cypress/e2e/carrinho/ct07_add_cart.cy.js
@@ -5,12 +5,20 @@ describe('Página de login Saucedemo', () => {
     cy.visit('/'); // antes do caso teste, visitando a url.
   });
 
-  it('Login válido', () => {
+  it('Adicionar ao Carrinho e testar notificação da quantidade no cart', () => {
 
     cy.get('[data-test="username"]').type("standard_user");   //inserindo nomes
     cy.get('[data-test="password"]').type("secret_sauce");    //inserindo senha   
     cy.get('[data-test="login-button"]').click();             //clicando
 
-  })
 
+     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+      cy.get('[data-test="shopping-cart-badge"]')
+        .should("exist")
+        .and("be.visible")
+        .and('have.text','1');
+
+
+  })
+  
 }) 
